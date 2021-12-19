d3a8204b83e139306b9408ccc254b559d5e9ccc6
Yousef <72878758+yousef647@users.noreply.github.com>
2021-12-20 00:20:36 +0200

    Delete PERIODIC TABLE directory

--- PERIODIC TABLE/main.js
+++
@@ -1,8 +0,0 @@
-// function about_element(){
-//   inner("<div class='modal-dialog modal-dialog-centered'>hello</div>")
-// }
-
-// const iron=document.getElementById("iron");
-// iron.onclick=function(){
-//   document.getElementById("em").innerHTML="<div class='modal-dialog modal-dialog-centered'>hello</div>"
-// }
