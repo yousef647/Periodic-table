b93572425332d8d1dc3523d190b2ff95c2ea790f
Yousef <72878758+yousef647@users.noreply.github.com>
2021-12-20 00:21:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// function about_element(){
+//   inner("<div class='modal-dialog modal-dialog-centered'>hello</div>")
+// }
+
+// const iron=document.getElementById("iron");
+// iron.onclick=function(){
+//   document.getElementById("em").innerHTML="<div class='modal-dialog modal-dialog-centered'>hello</div>"
+// }
